c4844b2fee07ca3a32aa627ae788f2077c654972
Tom Lebrun <tomlebrun27@gmail.com>
2021-10-02 14:26:47 +0200

    <Feat>Add avis
    création form avis

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -5,8 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use\App\Entity\Avis;
-use\App\Repository\AvisRepository;
+use App\Entity\Avis;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+use App\Repository\AvisRepository;
+
 class AvisController extends AbstractController
 {
     /**
@@ -23,14 +28,32 @@ class AvisController extends AbstractController
     }
 
     /**
+     * @Route("/avis/nouveau", name="avis_nouveau")
+     */
+    public function createAvis(Request $request, EntityManagerInterface $manager){
+        $avis = new Avis();
+
+        $form = $this->createFormBuilder($avis)
+                    ->add('titre')
+                    ->add('auteur')
+                    ->add('content')
+                    ->getForm();
+
+        return $this->render('avis/createAvis.html.twig',[
+            'formAvis' => $form->createView()
+        ]);
+    }
+
+
+    /**
      * @Route("/avis/{id}", name="avis_show")
      */
     public function show(Avis $avi){
         
-        
-
         return $this->render('avis/show.html.twig', [
             'avi' => $avi
         ]);
     }
+
+    
 }
